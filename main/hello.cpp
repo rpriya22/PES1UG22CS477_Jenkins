98b38f555ba2d064fffe96c381e7dd3450fc72a8
rpriya22 <159934155+rpriya22@users.noreply.github.com>
2025-03-18 16:36:25 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "PES1UG22CS477_Rishita  \n";
+  cout << "PES1UG22CS526_Sanjana  \n";
   cout << "Jenkins  \n";
   return 0;
 }
